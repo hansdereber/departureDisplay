61f6f8e705ad90e703d4483e5c0ba7ae5def2250
Christian Geisler <christian.geisler@posteo.de>
2018-11-19 02:49:20 +0100

    added station map and restart counter

--- central/central.ino
+++ central/central.ino
@@ -36,20 +36,25 @@ struct Connection
   uint32_t departureTime;
   uint8_t minutesToDeparture;
 };
+struct Connection connections[2];
+
+struct Map
+{
+  uint16_t stationId;
+  const char *name;
+};
+struct Map stationMap[6];
 
 // global variables
 WiFiClientSecure client;
-BLECharacteristic *pDisplayText;
 DateTime serverTime;
-struct Connection connections[2];
 int numOfConnections;
 static BLEAddress *pPeripheralAddress;
-BLEServer *pServer = NULL;
-BLEService *pService = NULL;
 BLECharacteristic *pTxCharacteristic;
-BLECharacteristic *pRxCharacteristic;
+BLEClient *pPeripheral;
 static boolean doConnect = false;
-static boolean connected = false;
+static boolean minutesChanged = true;
+uint8_t restartCounter = 0;
 
 #define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
@@ -59,12 +64,8 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
 {
   void onResult(BLEAdvertisedDevice advertisedDevice)
   {
-    Serial.print("BLE Advertised Device found: ");
-    Serial.println(advertisedDevice.toString().c_str());
-    Serial.println(advertisedDevice.getAddress().toString().c_str());
     if (advertisedDevice.getAddress().toString() == PERIPHERAL_ADDRESS)
     {
-      Serial.print("Found our device!  address: ");
       advertisedDevice.getScan()->stop();
       pPeripheralAddress = new BLEAddress(advertisedDevice.getAddress());
       doConnect = true;
@@ -74,20 +75,37 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
 
 void setup()
 {
+  initStationMap();
   Serial.begin(115200);
   initBluetoothLowEnergy();
-  Serial.println(F("Reserved space for JSON Object: "));
-  Serial.println(String(CAPACITY));
 }
 
-boolean isRelevantDestination(String destination)
+void initStationMap()
 {
-  return (destination == F("Ostbahnhof") ||
-          destination == F("Messestadt West") ||
-          destination == F("Karlsplatz (Stachus)") ||
-          destination == F("Trudering Bf.") ||
-          destination == F("Giesing Bf.") ||
-          destination == F("Ostfriedhof"));
+  stationMap[0].stationId = 1;
+  stationMap[0].name = "Karlsplatz (Stachus)";
+  stationMap[1].stationId = 5;
+  stationMap[1].name = "Ostbahnhof";
+  stationMap[2].stationId = 1250;
+  stationMap[2].name = "Messestadt West";
+  stationMap[3].stationId = 920;
+  stationMap[3].name = "Trudering Bf.";
+  stationMap[4].stationId = 1110;
+  stationMap[4].name = "Giesing Bf.";
+  stationMap[5].stationId = 1105;
+  stationMap[5].name = "Ostfriedhof";
+}
+
+boolean isRelevantDestination(const char *destination)
+{
+  for (int i = 0; i < sizeof(stationMap) / sizeof(stationMap[0]); i++)
+  {
+    if (strcmp(stationMap[i].name, destination) == 0)
+    {
+      return true;
+    }
+  }
+  return false;
 }
 
 void checkAndConnectToWifi()
@@ -116,54 +134,62 @@ void initBluetoothLowEnergy()
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true);
   pBLEScan->start(30);
-
-  pServer = BLEDevice::createServer();
-  pService = pServer->createService(SERVICE_UUID);
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
   pTxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
   pTxCharacteristic->addDescriptor(new BLE2902());
-  pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
+  BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
 
   pService->start();
 }
 
 void loop()
 {
-  if (doConnect == true)
-  {
-    if (connectToServer(*pPeripheralAddress))
-    {
-      Serial.println("We are now connected to the BLE Server.");
-      connected = true;
-    }
-    else
-    {
-      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
-    }
-    doConnect = false;
-  }
-
+  checkAndConnectToBle();
   checkAndConnectToWifi();
   Serial.println(F("Requesting departures"));
-  if (getDepartures())
+  if (getDepartures() && minutesChanged)
   {
-    Serial.println(F("Drawing departures"));
+    Serial.println(F("Sending departures"));
     sendDeparturesViaBle();
-    delay(10000);
   }
-  else
+  checkResetCounter();
+  delay(10000);
+}
+
+void checkAndConnectToBle()
+{
+  if (doConnect == true)
   {
-    delay(10000);
+    connectToServer(*pPeripheralAddress);
+    doConnect = false;
+  }
+  if (!pPeripheral->isConnected())
+  {
+    Serial.println(F("Peripheral not connected"));
+    restartCounter++;
+  }
+}
+
+void checkResetCounter()
+{
+  Serial.print(F("RestartCounter: "));
+  Serial.print(String(restartCounter));
+  Serial.println(F("/6"));
+
+  if (restartCounter > 6)
+  {
+    Serial.println(F("Restarting ESP"));
+    ESP.restart();
   }
 }
 
 bool connectToServer(BLEAddress pAddress)
 {
-  Serial.print("Forming a connection to ");
+  Serial.print(F("Forming a connection to "));
   Serial.println(pAddress.toString().c_str());
-  BLEClient *pClient = BLEDevice::createClient();
-  Serial.println(" - Created client");
-  pClient->connect(pAddress);
-  Serial.println(" - Connected to server");
+  pPeripheral = BLEDevice::createClient();
+  pPeripheral->connect(pAddress);
 }
 
 boolean getDepartures()
@@ -192,6 +218,7 @@ boolean connectAndSendRequest()
   if (!client.connect(HOST, 443))
   {
     Serial.println(F("Failed to connect"));
+    restartCounter++;
     return false;
   }
 
@@ -200,6 +227,7 @@ boolean connectAndSendRequest()
   if (client.println() == 0)
   {
     Serial.println(F("Failed to send request"));
+    restartCounter++;
     return false;
   }
 
@@ -214,6 +242,7 @@ boolean isStatusOk()
   {
     Serial.print(F("Unexpected response: "));
     Serial.println(status);
+    restartCounter++;
     return false;
   }
   return true;
@@ -230,8 +259,6 @@ boolean getDateFromHeader()
     int year = client.parseInt();   // year
     int hour = client.parseInt();   // hour
     int minute = client.parseInt(); // minute
-    Serial.print("Date found with minute: ");
-    Serial.println(String(minute));
     int second = client.parseInt(); // second
     int month;
     switch (buf[0])
@@ -292,6 +319,7 @@ boolean jumpToEndOfHeaders()
   if (!client.find("\r\n\r\n"))
   {
     Serial.println(F("Invalid response"));
+    restartCounter++;
     return false;
   }
   return true;
@@ -305,22 +333,34 @@ boolean extractDeparturesFromResponse()
   if (!response.success())
   {
     Serial.println(F("Parsing failed!"));
+    restartCounter++;
     return false;
   }
   JsonArray &departures = response[F("departures")];
   Serial.print(F("Number of departures: "));
   Serial.println(sizeof(departures));
+  minutesChanged = false;
   for (int i = 0; i < sizeof(departures) && numOfConnections < 2; i++)
   {
     const char *destination = departures[i]["destination"].as<char *>();
     if (isRelevantDestination(destination))
     {
       String departureTime = departures[i]["departureTime"].as<String>().substring(0, 10);
-      TimeSpan timespanToDeparture((DateTime) departureTime.toInt() - serverTime);
+      TimeSpan timespanToDeparture((DateTime)departureTime.toInt() - serverTime);
       String lineNumber = departures[i]["label"].as<char *>();
       connections[numOfConnections].line = departures[i]["label"].as<char *>();
-      connections[numOfConnections].destinationId = 1001;
+      connections[numOfConnections].destinationId = findStationIdByName(destination);
       connections[numOfConnections].departureTime = departureTime.toInt();
+      if (connections[numOfConnections].minutesToDeparture == timespanToDeparture.minutes() && !minutesChanged)
+      {
+        minutesChanged = false;
+      }
+      else
+      {
+        minutesChanged = true;
+      }
+      Serial.print(F("minutesChanged: "));
+      Serial.println(String(minutesChanged));
       connections[numOfConnections].minutesToDeparture = timespanToDeparture.minutes();
       numOfConnections++;
     }
@@ -331,41 +371,53 @@ boolean extractDeparturesFromResponse()
   return true;
 }
 
+uint16_t findStationIdByName(const char *str)
+{
+  for (int i = 0; i < sizeof(stationMap) / sizeof(stationMap[0]); i++)
+  {
+    if (strcmp(stationMap[i].name, str) == 0)
+    {
+      return stationMap[i].stationId;
+    }
+  }
+  return 0;
+}
+
 void sendDeparturesViaBle()
 {
-  uint8_t* line1 = (uint8_t*) connections[0].line;
+  uint8_t *line1 = (uint8_t *)connections[0].line;
   uint16_t stationId1 = connections[0].destinationId;
   uint32_t departureTime1 = connections[0].departureTime;
   uint8_t timeTillDeparture1 = connections[0].minutesToDeparture;
-  uint8_t* line2 = (uint8_t*) connections[1].line;
+  uint8_t *line2 = (uint8_t *)connections[1].line;
   uint16_t stationId2 = connections[1].destinationId;
   uint32_t departureTime2 = connections[1].departureTime;
   uint8_t timeTillDeparture2 = connections[1].minutesToDeparture;
 
-	uint8_t payload[20];
-  payload[0]=line1[0];
-  payload[1]=line1[1];
-  payload[2]=line1[2];
-	payload[3]=stationId1;
-	payload[4]=stationId1>>8;
-  payload[5]=departureTime1;
-  payload[6]=departureTime1>>8;
-  payload[7]=departureTime1>>16;
-  payload[8]=departureTime1>>24;
-  payload[9]=timeTillDeparture1;
-  payload[10]=line2[0];
-  payload[11]=line2[1];
-  payload[12]=line2[2];
-	payload[13]=stationId2;
-	payload[14]=stationId2>>8;
-  payload[15]=departureTime2;
-  payload[16]=departureTime2>>8;
-  payload[17]=departureTime2>>16;
-  payload[18]=departureTime2>>24;
-  payload[19]=timeTillDeparture2;
+  uint8_t payload[20];
+  payload[0] = line1[0];
+  payload[1] = line1[1];
+  payload[2] = line1[2];
+  payload[3] = stationId1;
+  payload[4] = stationId1 >> 8;
+  payload[5] = departureTime1;
+  payload[6] = departureTime1 >> 8;
+  payload[7] = departureTime1 >> 16;
+  payload[8] = departureTime1 >> 24;
+  payload[9] = timeTillDeparture1;
+  payload[10] = line2[0];
+  payload[11] = line2[1];
+  payload[12] = line2[2];
+  payload[13] = stationId2;
+  payload[14] = stationId2 >> 8;
+  payload[15] = departureTime2;
+  payload[16] = departureTime2 >> 8;
+  payload[17] = departureTime2 >> 16;
+  payload[18] = departureTime2 >> 24;
+  payload[19] = timeTillDeparture2;
 
-	pTxCharacteristic->setValue(payload, 20);
+  pTxCharacteristic->setValue(payload, 20);
 
-  pTxCharacteristic->setValue((uint8_t*) payload, 20);
+  pTxCharacteristic->setValue((uint8_t *)payload, 20);
   pTxCharacteristic->notify();
 }
